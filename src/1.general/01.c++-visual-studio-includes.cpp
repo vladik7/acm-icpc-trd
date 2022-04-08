a8061c93a8312b10b0322d63a5a718fb448bc1bf
linsierra <linsierra@yandex.ru>
2022-04-08 15:59:04 +0300

    Update VS template

--- src/1.general/01.c++-visual-studio-includes.cpp
+++ src/1.general/01.c++-visual-studio-includes.cpp
@@ -1,27 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS
-#pragma comment(linker, "/STACK:167772160000")
 #include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <cstdio>
-#include <cstdlib>
-#include <cassert>
-#include <climits>
-#include <cmath>
-#include <algorithm>
-#include <cstring>
-#include <string>
 #include <vector>
-#include <list>
-#include <stack>
+#include <string>
+#include <algorithm>
 #include <set>
-#include <bitset>
-#include <queue>
 #include <map>
-#include <sstream>
-#include <functional>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+#include <bitset>
 #include <unordered_map>
-#include <unordered_set>
+#include <stack>
+#include <memory.h>
+#include <list>
+#include <numeric>
+#include <functional>
 #include <complex>
+#include <cassert>
+#include <regex>
 #include <random>
-#include <chrono>
+#include <iomanip>
+#include <climits>
+#pragma comment(linker, "/STACK:360777216")
+
+using LL = long long;
+using ld = long double;
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define pii pair<int,int> 
+#define pll pair<LL,LL> 
+#define vi vector<int>
+#define vll vector<LL>
+#define vvll vector<vector<LL>>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vvi vector<vector<int>>
+#define forn(it,from,to) for(int (it)=from; (it)<to; (it)++)
+const int  INF = 2 * 1000 * 1000 * 1000;
+LL MOD = 1e9 + 7;
+LL LINF = (LL)4e18;
+double EPS = 1e-7;
+
+using namespace std;
+
+
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#else
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cout << fixed << setprecision(10);
+	srand(time(nullptr));
+
+	LL __ = 1, n, m, k, r, u, v, m1, m2, x, y, l, a, b;
+
+
+	cin >> __;
+	
+	forn(_, 0, __)
+	{
+		cin >> n;
+		
+
+	}
+
+
+	return 0;
+}
