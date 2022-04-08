d85041a17e8bbdd4142651b50047fd99ea1c1f19
linsierra <linsierra@yandex.ru>
2022-04-08 16:02:42 +0300

    Update dijkstra

--- src/3.graphs/14.dijkstra.cpp
+++ src/3.graphs/14.dijkstra.cpp
@@ -1,25 +1,24 @@
-vector<vector<pair<int, int>>> adj;
-void dijkstra(int s, vector<int> & d, vector<int> & p) {
-    int n = adj.size();
-    d.assign(n, oo);
-    p.assign(n, -1);
-
-    d[s] = 0;
-    min_heap<pii> q;
-    q.push({0, s});
-    while (!q.empty()) {
-        int v = q.top().second;
-        int d_v = q.top().first;
-        q.pop();
-        if (d_v != d[v]) continue;
-        for (auto edge : adj[v]) {
-            int to = edge.first;
-            int len = edge.second;
-            if (d[v] + len < d[to]) {
-                d[to] = d[v] + len;
-                p[to] = v;
-                q.push({d[to], to});
-            }
-        }
-    }
+void dijkstra(vector<vector<pll>>& g, vi& p, vll& d, int start)
+{
+	priority_queue<pll, vector<pll>, greater<pll>> q;
+	d[start] = 0;
+	q.push({ 0, start });
+	while (!q.empty())
+	{
+		auto from = q.top().second;
+		auto dist = q.top().first;
+		q.pop();
+		if (dist > d[from]) continue;
+		for (auto& cur : g[from])
+		{
+			auto to = cur.second;
+			auto to_dist = cur.first;
+			if (d[from] + to_dist < d[to])
+			{
+				d[to] = d[from] + to_dist;
+				p[to] = from;
+				q.push({ d[to], to });
+			}
+		}
+	}
 }
